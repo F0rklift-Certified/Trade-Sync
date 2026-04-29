23e3f77ff8629f10804e472a5f1a3829ec7d8292
F0rklift-Certified <noah.bondoc11@gmail.com>
2026-04-29 15:46:54 +1000

    Updating from local files

---
+++ Trade Sync/Trade_SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trade_SyncApp.swift
+//  Trade Sync
+//
+//  Created by NB on 28/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct Trade_SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
